2fd9b9a59c65f07b33253ac9e1018925f9445569
Ali-mahdiyanjoo <mahdiaynjooali@gmail.com>
2022-04-19 20:04:59 +0430

    it would run all code

--- All_seervice.py
+++ All_seervice.py
@@ -1,4 +1,4 @@
 # run all service
 import os
 
-os.system("python3 main.py && python3 Genral_heamap.py && pyhton3 Advance_IP.py")
+os.system("python3 main.py & python3 Genral_heamap.py & pyhton3 Advance_IP.py")
