436125aa2a128fe12ba1b41710e79c06e77da2a4
Ali-mahdiyanjoo <mahdiaynjooali@gmail.com>
2022-05-14 16:22:28 +0430

    path has been correct

--- All_seervice.py
+++ All_seervice.py
@@ -1,4 +1,4 @@
 # run all service
 import os
 
-os.system("python3 main.py & python3 Genral_heamap.py & pyhton3 Advance_IP.py")
+os.system("python3 /home/ubuntu/GPU_Monitoring/Virtualizaing/main.py & python3 /home/ubuntu/GPU_Monitoring/Virtualizaing/Genral_heatmap.py & python3 /home/ubuntu/GPU_Monitoring/Virtualizaing/Advance_IP.py")
